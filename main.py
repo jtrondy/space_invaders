5154b3d792abfa235f681d3071cb3b85c4c4f96f
Jared Stevenson <73672469+jtrondy@users.noreply.github.com>
2022-07-12 21:07:43 +1200

    ship class template

--- main.py
+++ main.py
@@ -27,12 +27,28 @@ YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"
 BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
 
 
+class Ship:
+    def __init__(self, x, y, health=100):
+        self.x = x
+        self.y = y
+        self.health = health
+        self.ship_img = None
+        self.laser_img = None
+        self.lasers = []
+        self.cool_down_counter = 0
+
+    def draw(self, window):
+        pygame.draw.rect(window, (255, 0, 0), (self.x, self.y, 50, 50), 0)
+
+
 def main():
     run = True
     fps = 60
     level = 1
     lives = 5
     main_font = pygame.font.SysFont("comicsans", 50)
+
+    ship = Ship(300, 650)
 
     clock = pygame.time.Clock()
 
@@ -44,6 +60,8 @@ def main():
         level_label = main_font.render(f"Level: {level}", 1, (255, 255, 255))
         WIN.blit(lives_label, (10, 10))
         WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
+
+        ship.draw(WIN)
 
         pygame.display.update()
 
