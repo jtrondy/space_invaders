39ef3a109f153013ee31f66cf3528b879c6a04a9
Jared Stevenson <73672469+jtrondy@users.noreply.github.com>
2022-07-12 20:41:22 +1200

    importing modules & loading images

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import pygame
+import os
+import time
+import random
 
+# loading images
+RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
+GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
+BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
 
+# Player ship
+YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
 
+# Lasers
+RED_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
+GREEN_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
+BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
+YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
 
-if __name__ == '__main__':
+# bg images
+BG = pygame.image.load(os.path.join("assets", "background-black.png"))
+
+# if __name__ == '__main__':
