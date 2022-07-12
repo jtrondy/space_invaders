32cb5caccc94496373b04a7eb2bf785c35431ab2
Jared Stevenson <73672469+jtrondy@users.noreply.github.com>
2022-07-12 20:47:14 +1200

    main loop, quit event

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import os
 import time
 import random
 
+WIDTH, HEIGHT = 750, 750
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space Shooter")
+
+
 # loading images
 RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
 GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
@@ -17,7 +22,22 @@ GREEN_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
 BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
 YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
 
-# bg images
+# Background
 BG = pygame.image.load(os.path.join("assets", "background-black.png"))
 
-# if __name__ == '__main__':
+
+def main():
+    run = True
+    FPS = 60
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+
+if __name__ == '__main__':
+    main()
