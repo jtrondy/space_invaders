32dd52206924c7995baafbc5287580432d2b1a76
Jared Stevenson <73672469+jtrondy@users.noreply.github.com>
2022-07-12 20:55:21 +1200

    background image drawn

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import os
 import time
 import random
+pygame.font.init()
 
 WIDTH, HEIGHT = 750, 750
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -23,16 +24,25 @@ BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
 YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
 
 # Background
-BG = pygame.image.load(os.path.join("assets", "background-black.png"))
+BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
 
 
 def main():
     run = True
-    FPS = 60
+    fps = 60
+    level = 1
+    lives = 5
+
     clock = pygame.time.Clock()
 
+    # local scope function
+    def redraw_window():
+        WIN.blit(BG, (0, 0))  # top left of window
+        pygame.display.update()
+
     while run:
-        clock.tick(FPS)
+        clock.tick(fps)
+        redraw_window()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
